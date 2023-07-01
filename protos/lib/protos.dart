0afccf7069e7581e5b57a77b139fd456378f7334
brahim tuijine <brahim.tuijne@slnee.com>
2023-07-01 19:12:52 +0100

    generate protos files done

--- protos/lib/protos.dart
+++ protos/lib/protos.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library;
 
-export 'src/protos_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/generated/todo.pb.dart';
+export 'src/generated/todo.pbenum.dart';
+export 'src/generated/todo.pbjson.dart';
